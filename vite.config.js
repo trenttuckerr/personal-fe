98feabf416ae277e3e41adff66e1399f813d3327
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2023-08-16 14:14:01 -0500

    vite + react scaffolding

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // Vite configuration options
-});
+  plugins: [react()],
+})
