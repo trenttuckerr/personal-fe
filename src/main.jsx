e1e930abe34421d3b371dde95f8a4b99beec7491
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2023-08-17 22:23:42 -0500

    Clean

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom';
 import App from './App.jsx';
 import './index.css';
-// Run Ci-CD pipeline
+
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
