3a58673ce9bd0262427dfc37d766a94f8c071746
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2023-09-14 15:44:17 -0500

    mocking additions

---
+++ src/mock.js
@@ -0,0 +1,7 @@
+const svgMock = 'test-svg-content';
+const cssMock = 'test-css-content';
+const mp4Mock = 'test-mp4-content';
+const webmMock = 'test-webm-content';
+const pngMock = 'test-png-content';
+
+export { svgMock, cssMock, mp4Mock, webmMock, pngMock };
