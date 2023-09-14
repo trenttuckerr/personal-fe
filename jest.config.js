e4c4d10d528fe2111255f71f3cb9f84a29c947b2
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2023-09-14 15:43:43 -0500

    Add mocking mappers for utilized extensions

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,18 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    transform: {
-      '^.+\\.jsx?$': 'babel-jest',
-    },
-    moduleNameMapper: {
-      '\\.(svg)$': '<rootDir>/src/mock.js',
-      '\\.(css)$': '<rootDir>/src/mock.js',
-    },
-    testEnvironment: 'jsdom',
-    testMatch: ['<rootDir>/tests/**/*.test.(js|jsx)'],
-    collectCoverage: true,
-    collectCoverageFrom: ['src/**/*.jsx'],
-  };
-  
+  transform: {
+    '^.+\\.jsx?$': 'babel-jest',
+  },
+
+  moduleNameMapper: {
+    '\\.(svg)$': '<rootDir>/src/mock.js',
+    '\\.(css)$': '<rootDir>/src/mock.js',
+    '\\.(mp4)$': '<rootDir>/src/mock.js',
+    '\\.(webm)$': '<rootDir>/src/mock.js',
+    '\\.(png)$': '<rootDir>/src/mock.js',
+  },
+  testEnvironment: 'jsdom',
+  testMatch: ['<rootDir>/tests/**/*.test.(js|jsx)'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.jsx'],
+};
