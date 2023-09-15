a30995716f920f8478274b8e3c8e42ce189955b2
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2023-09-15 12:22:10 -0500

    Revamp skills section - Images - Content

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,39 +1,71 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import '../styles/Skills.css';
+import ProgrammingLanguageImage from '../assets/ProgrammingLanguages.jpg';
+import CloudImage from '../assets/Cloud.jpg';
+import DevOpsImage from '../assets/DevOps.jpg';
 
 function Skills() {
   return (
     <section className="skills" id="skills">
-      <div className="skills-content">
-        <h2 className="skills-title">Skills</h2>
-        <div className="skills-list">
-          <div className="skill-category">
-            <h3>Programming Languages</h3>
-            <ul>
-              <li>Python</li>
-              <li>Java</li>
-              <li>JavaScript</li>
-              <li>TypeScript</li>
-            </ul>
+      <div className="skills-container">
+        <h2 className="skills-title">Technical Skills</h2>
+        <div className="skills-table">
+          <div className="table-row" data-aos="fade-up">
+            <div className="table-cell">
+              <h3>Development</h3>
+              <div className="category-image">
+                <img src={ProgrammingLanguageImage} alt="Programming Language Visual" />
+              </div>
+            </div>
+            <div className="table-cell">
+              <ul>
+                <li>🐍 Python</li>
+                <li>☕ Java</li>
+                <li>🚀 JavaScript</li>
+                <li>📘 TypeScript</li>
+                <li>⚛️ React</li>
+                <li>🌐 Node.js</li>
+                <li>🌟 Express.js</li>
+                <li>🌶️ Flask</li>
+                <li>🔗 API Design and Development</li>
+                <li>📜 Git</li>
+              </ul>
+            </div>
           </div>
-          <div className="skill-category">
-            <h3>Cloud</h3>
-            <ul>
-              <li>Serverless Architecture</li>
-              <li>AWS</li>
-              <li>Azure</li>
-              <li>Google Cloud</li>
-            </ul>
+          <div className="table-row" data-aos="fade-up">
+            <div className="table-cell">
+              <h3>Cloud and Infrastructure</h3>
+              <div className="category-image">
+                <img src={CloudImage} alt="Cloud Visual" />
+              </div>
+            </div>
+            <div className="table-cell">
+              <ul>
+                <li>☁️ AWS (Amazon Web Services)</li>
+                <li>🌐 Azure</li>
+                <li>☁️ Google Cloud</li>
+                <li>🐳 Docker</li>
+                <li>☸️ Kubernetes</li>
+                <li>🌍 Terraform</li>
+              </ul>
+            </div>
           </div>
-          <div className="skill-category">
-            <h3>Data Structures</h3>
-            <ul>
-              <li>Arrays</li>
-              <li>Linked Lists</li>
-              <li>Trees</li>
-              <li>HashMaps</li>
-            </ul>
+          <div className="table-row" data-aos="fade-up">
+            <div className="table-cell">
+              <h3>DevOps and Tools</h3>
+              <div className="category-image">
+                <img src={DevOpsImage} alt="DevOps Visual" />
+              </div>
+            </div>
+            <div className="table-cell">
+              <ul>
+                <li>🛠️ CI/CD Pipelines</li>
+                <li>🐳 Containerization with Docker</li>
+                <li>🏗️ Infrastructure as Code using AWS CDK</li>
+                <li>📊 Monitoring and Logging with AWS CloudWatch</li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
