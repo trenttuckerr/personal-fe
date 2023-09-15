0990e734c9ff5360c9bec5a8c9a282479478694a
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2023-09-15 12:33:59 -0500

    try old naming convention

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React, { useState, useEffect } from 'react';
-import backgroundVideoWebm from '../assets/AI.webm';
+import backgroundVideoWebm from '../assets/ai.webm';
 import backgroundVideoMp4 from '../assets/AI.mp4';
 import '../styles/Hero.css';
 
