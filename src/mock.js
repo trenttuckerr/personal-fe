f3bc9a72b0b4f5b9473e11274d05042392717e6a
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2023-09-15 12:27:22 -0500

    Jpg additions

--- src/mock.js
+++ src/mock.js
@@ -3,5 +3,6 @@ const cssMock = 'test-css-content';
 const mp4Mock = 'test-mp4-content';
 const webmMock = 'test-webm-content';
 const pngMock = 'test-png-content';
+const jpgMock = 'test-jpg-content';
 
-export { svgMock, cssMock, mp4Mock, webmMock, pngMock };
+export { svgMock, cssMock, mp4Mock, webmMock, pngMock, jpgMock };
