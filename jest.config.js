b6aea488f0bffe8756d5210387decfbb1773b4c8
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2023-10-05 17:50:54 -0500

    configuration fixes

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,12 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-
-  moduleNameMapper: {
-    '\\.(svg)$': '<rootDir>/src/mock.js',
-    '\\.(css)$': '<rootDir>/src/mock.js',
-    '\\.(mp4)$': '<rootDir>/src/mock.js',
-    '\\.(webm)$': '<rootDir>/src/mock.js',
-    '\\.(png)$': '<rootDir>/src/mock.js',
-  },
-  testEnvironment: 'jsdom',
-  testMatch: ['<rootDir>/tests/**/*.test.js'],
-  collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.jsx'],
-  coveragePathIgnorePatterns: ['src/main.jsx'],
-};
+    testEnvironment: 'jsdom',
+    // Define the file patterns Jest should look for when running tests
+    testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+    // Set up the module name mapper to handle assets, CSS, etc.
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+        '<rootDir>/src/mock.js',
+    },
+  };
