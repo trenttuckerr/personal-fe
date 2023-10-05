175a3f4cb257fa9bfe2b2862a63234ed6879f66b
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2023-10-05 17:51:19 -0500

    Restructure experience

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import '../styles/Experience.css';
 
@@ -7,55 +6,59 @@ function Experience() {
     <section className="experience" id="experience">
       <div className="experience-content">
         <h2 className="experience-title">Professional Experience</h2>
-        <div className="jobs">
-          <h3>Engineering Summer Analyst</h3>
-          <h4>Goldman Sachs</h4>
+        <div className="job">
+          <h3 className="job-title">Engineering Summer Analyst</h3>
+          <p className="job-company">Goldman Sachs</p>
+          <p className="job-location">New York City Metropolitan Area</p>
           <p className="job-date">June 2023 - August 2023</p>
           <div className="job-description">
             <p>
               Designed database schema, implemented backend business
-              logic, and deployed cloud infrastructure for security
-              key management application
+              logic, and deployed cloud infrastructure for a security
+              key management application.
             </p>
-            <ul>
+            <ul className="job-highlights">
               <li>
-                Coded serverless AWS Lambda functions to handle
-                requests for database migrations and CRUD operations
+                Developed serverless AWS Lambda functions to handle
+                requests for database migrations and CRUD operations.
               </li>
               <li>
-                Utilized Flask to design serverless, event-driven
+                Utilized Flask to design a serverless, event-driven
                 backend that uses AWS Lambda functions for handling
-                CRUD API requests and database migrations
+                CRUD API requests and database migrations.
               </li>
               <li>
                 Developed Infrastructure as Code to deploy an
                 Application Load Balancer, Aurora PostgreSQL database,
                 Lambda functions, and security groups to AWS to
-                support application's cloud environment
+                support the application's cloud environment.
               </li>
             </ul>
           </div>
-          <h3>Engineering Summer Analyst</h3>
-          <h4>Goldman Sachs</h4>
+        </div>
+        <div className="job">
+          <h3 className="job-title">Engineering Summer Analyst</h3>
+          <p className="job-company">Goldman Sachs</p>
+          <p className="job-location">New York City Metropolitan Area</p>
           <p className="job-date">June 2022 - August 2022</p>
           <div className="job-description">
             <p>
-              Developed a microservice which orchestrates data event
-              routing logic for deferred compensation workflow
+              Developed a microservice that orchestrates data event
+              routing logic for the deferred compensation workflow.
             </p>
-            <ul>
+            <ul className="job-highlights">
               <li>
-                Utilized distributed, event-driven system Apache Kafka
-                as streaming platform for managing data life cycle
+                Utilized a distributed, event-driven system with Apache Kafka
+                as a streaming platform for managing data lifecycle.
               </li>
               <li>
                 Created a Node.js project using Typescript for
-                development and Jest for testing
+                development and Jest for testing.
               </li>
               <li>
                 Used Infrastructure as Code to upload the service to
                 AWS Lambda, with invocation available via AWS API
-                Gateway
+                Gateway.
               </li>
             </ul>
           </div>
