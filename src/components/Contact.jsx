fb51e5d327d6ad8f2e2e7a422d247796634557bf
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2023-10-05 17:53:28 -0500

    new link button with good color

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,21 +1,30 @@
-/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
 import '../styles/Contact.css';
 
 function Contact() {
   return (
     <section className="contact" id="contact">
-      <div className="contact-content">
-        <h2 className="contact-title">Contact</h2>
-        <p className="contact-description">
-          Feel free to reach out.
-        </p>
-        <a
-          href="mailto:ttucker296@gmail.com"
-          className="contact-button"
-        >
-          Email
-        </a>
+      <div className="contact-box">
+        <div className="contact-content">
+          <h2 className="contact-title">Contact</h2>
+          <p className="contact-description">
+            Feel free to reach out.
+          </p>
+          <div className="contact-buttons">
+            <a
+              href="mailto:ttucker296@gmail.com"
+              className="contact-button"
+            >
+              Email
+            </a>
+            <a
+              href="https://www.linkedin.com/in/-trent-tucker"
+              className="contact-button linkedin-button"
+            >
+              LinkedIn
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
