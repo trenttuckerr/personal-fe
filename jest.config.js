8cdd2de400a8a9abb6f5ee064a4aee92521e9ad3
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2023-10-05 18:38:11 -0500

    format

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
-// eslint-disable-next-line no-undef
 module.exports = {
-    testEnvironment: 'jsdom',
-    // Define the file patterns Jest should look for when running tests
-    testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
-    // Set up the module name mapper to handle assets, CSS, etc.
-    moduleNameMapper: {
-      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-      '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-        '<rootDir>/src/mock.js',
-    },
-  };
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/mock.js',
+  },
+};
