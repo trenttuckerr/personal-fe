8bc0c05389031aa3034fd2cd6cd4ee643d47c280
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2023-10-06 13:08:17 -0500

    coverage directory edit

--- jest.config.js
+++ jest.config.js
@@ -6,4 +6,6 @@ module.exports = {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/src/mock.js',
   },
+  collectCoverage: true,
+  coverageDirectory: '.nyc_output',
 };
