96a81c810ba8621e57cc31bed911eb4bf20af515
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2023-10-06 13:08:53 -0500

    further test cases for interval

--- tests/Hero.test.js
+++ tests/Hero.test.js
@@ -3,39 +3,58 @@ import { render } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Hero from '../src/components/Hero';
 
-describe('Hero Component', () => {
-  it('renders without crashing', () => {
+jest.useFakeTimers();
+
+describe('Hero component', () => {
+  let intervalId;
+
+  beforeEach(() => {
+    intervalId = setInterval(() => {}, 1000);
+  });
+
+  afterEach(() => {
+    clearInterval(intervalId);
+  });
+
+  it('should render the component without errors', () => {
     const { container } = render(<Hero />);
-    expect(container).toBeInTheDocument();
+    expect(container).toBeDefined();
   });
 
-  it('has the correct class name', () => {
+  it('should start with popIn state as false', () => {
     const { container } = render(<Hero />);
-    const hero = container.querySelector('.hero');
-    expect(hero).toHaveClass('hero');
+    const heroContent = container.querySelector('.hero-content');
+    expect(heroContent).not.toHaveClass('pop-in');
   });
 
-  it('renders the hero title', () => {
-    const { getByText } = render(<Hero />);
-    const heroTitle = getByText('Welcome to my personal website');
-    expect(heroTitle).toBeInTheDocument();
-  });
-
-  it('renders the "View website code" button', () => {
-    const { getByText } = render(<Hero />);
-    const viewCodeButton = getByText('View website code');
-    expect(viewCodeButton).toBeInTheDocument();
-  });
-
-  it('has the "pop-in" animation class when "popIn" is true', () => {
+  it('should set popIn state to true after 500ms', () => {
     const { container } = render(<Hero />);
+    jest.advanceTimersByTime(500);
     const heroContent = container.querySelector('.hero-content');
     expect(heroContent).toHaveClass('hero-content');
   });
 
-  it('has the typing cursor', () => {
-    const { container } = render(<Hero />);
-    const typingCursor = container.querySelector('.typing-cursor');
-    expect(typingCursor).toBeInTheDocument();
+  it('should render the View website code link with the correct URL', () => {
+    const { getByText } = render(<Hero />);
+    const linkElement = getByText('View website code');
+    expect(linkElement).toHaveAttribute(
+      'href',
+      'https://github.com/trenttuckerr/mern-personal-fe'
+    );
+  });
+
+  it('should clear the interval when the component unmounts', () => {
+    let intervalId;
+    const originalClearInterval = clearInterval;
+    // eslint-disable-next-line no-global-assign
+    clearInterval = (id) => {
+      intervalId = id;
+      originalClearInterval(id);
+    };
+    const { unmount } = render(<Hero />);
+    unmount();
+    // eslint-disable-next-line no-global-assign
+    clearInterval = originalClearInterval;
+    expect(typeof intervalId).toBe('number');
   });
 });
