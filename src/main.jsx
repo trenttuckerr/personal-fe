d6334f6dff732986d834bd3a3f184cc2069dacb1
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2024-01-11 12:39:37 -0600

    prep for new components

--- src/main.jsx
+++ src/main.jsx
@@ -1,26 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Header from './components/Header.jsx';
-import Hero from './components/Hero.jsx';
-import About from './components/About.jsx';
-import Skills from './components/Skills.jsx';
-import Projects from './components/Projects.jsx';
-import Experience from './components/Experience.jsx';
-import Contact from './components/Contact.jsx';
-import Footer from './components/Footer.jsx';
-import Background from './components/Background.jsx';
 import './styles/index.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Background />
-    <Header />
-    <Hero />
-    <About />
-    <Experience />
-    <Projects />
-    <Skills />
-    <Contact />
-    <Footer />
   </React.StrictMode>
 );
