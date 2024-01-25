831e26a51eab2431b862a0c2b2b8888058d348cf
Trent Tucker <97367913+trenttuckerr@users.noreply.github.com>
2024-01-25 04:28:29 -0600

    no more mock

--- src/mock.js
+++
@@ -1,8 +0,0 @@
-// const svgMock = 'test-svg-content';
-// const cssMock = 'test-css-content';
-// const mp4Mock = 'test-mp4-content';
-// const webmMock = 'test-webm-content';
-// const pngMock = 'test-png-content';
-// const jpgMock = 'test-jpg-content';
-
-// export { svgMock, cssMock, mp4Mock, webmMock, pngMock, jpgMock };
